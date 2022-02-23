cf38b272ca02b25001af671bf0d368dba95687bb
Bartosz Szajor <78597883+szajorb@users.noreply.github.com>
2022-02-23 12:16:27 +0100

    Add files via upload

--- src/com/company/Calculate.java
+++ src/com/company/Calculate.java
@@ -5,7 +5,7 @@ public class Calculate {
     public double calculateDistance(Point3D sun, Point3D earth )
     {
         return Math.sqrt(
-                Math.pow(sun.pozX - earth.pozX, 2) + Math.pow(sun.pozY - earth.pozY, 2) + Math.pow(sun.pozZ - earth.pozZ, 2)
+                Math.pow(sun.pozX - earth.pozX, 2) + Math.pow(sun.pozY - earth.pozY,2) + Math.pow(sun.pozZ - earth.pozZ, 2)
         );
     }
 }
--- src/com/company/Point3D.java
+++ src/com/company/Point3D.java
@@ -1,14 +1,14 @@
 package com.company;
 
 public class Point3D {
-    double pozX, pozY, pozZ;
+    double pozX,pozY,pozZ;
 
     public Point3D(double pozX,
                    double pozY,
                    double pozZ ) {
         this.pozX = pozX;
         this.pozY = pozY;
-        this.pozZ = pozZ;
+        this.pozZ=pozZ;
     }
 }
 
